f18c351d7760d35b33adf469934622b488073c4e
manvithasm <pavanputra4me@gmail.com>
2026-02-20 07:53:15 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Om Namah Shivaya.
+
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def Home():
+    return render_template('home.html')
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
